870e881a6d7e7fce629aa9705fbcf5aae8fa1456
RICHARD WESLEY BARBOSA FREITAS <wesleyrichard30@gmail.com>
2023-01-13 15:21:35 -0300

    Initial commit

---
+++ src/MenuController.java
@@ -0,0 +1,58 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MenuController {
+
+    @FXML
+    private Button botaoCadastro;
+
+    @FXML
+    private Button botaoEdicao;
+
+    @FXML
+    private Button botaoFinalizar;
+
+    @FXML
+    private Button botaoRemocao;
+
+    @FXML
+    private Button botaoReposicao;
+
+    @FXML
+    private Button botaoVenda;
+
+    @FXML
+    private Button botaoVisualizacao;
+
+    @FXML
+    void cadastrarProdutos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editarProdutos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void removerProdutos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reportProdutos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void venderProdutos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void visualizarProdutos(ActionEvent event) {
+
+    }
+
+}
